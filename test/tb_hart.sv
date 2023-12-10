ab932b2314f0be444421a15a09eb77fb6b572eb7
kiclu <kiclu@protonmail.com>
2023-12-10 01:08:53 +0100

    tb_hart update

--- test/tb_hart.sv
+++ test/tb_hart.sv
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2023  Nikola Lukic <lukicn@protonmail.com>
+ * This source describes Open Hardware and is licensed under the CERN-OHL-W v2
+ *
+ * You may redistribute and modify this documentation and make products
+ * using it under the terms of the CERN-OHL-W v2 (https:/cern.ch/cern-ohl).
+ * This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN-OHL-W v2
+ * for applicable conditions.
+ *
+ * Source location: https://www.github.com/kiclu/rv6
+ *
+ * As per CERN-OHL-W v2 section 4.1, should You produce hardware based on
+ * these sources, You must maintain the Source Location visible on the
+ * external case of any product you make using this documentation.
+ */
+
 `define hex_path "../test/c/fib/fib.hex"
 
 `define tb_mem_size  32'h0001_0000
