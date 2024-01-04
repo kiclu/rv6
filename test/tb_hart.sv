b2ade987397096eaddc92e848021e2749c005b00
kiclu <kiclu@protonmail.com>
2024-01-04 01:28:14 +0100

    riscv-tests verification framework

--- test/tb_hart.sv
+++ test/tb_hart.sv
@@ -21,6 +21,8 @@
 `define tb_entry    64'h8000_0000
 `define tb_mem_size 64'h10_0000
 
+`define ANSI_COLORS
+
 `timescale 1ns/1ps
 
 module tb_hart;
@@ -73,8 +75,7 @@ module tb_hart;
 
     // reset signal
     initial begin
-        h_rst_n = 0;
-        #80 h_rst_n = 1;
+        h_rst_n = 1;
     end
 
     // clock generator
@@ -126,40 +127,11 @@ module tb_hart;
 
     /* MONITOR */
 
-    integer fd;
-    initial begin
-        fd = $fopen("trace", "w");
-        $fdisplay(
-            fd,
-"0 3 0x0000000000010000 (0xf1402573) x10 0x0000000000000000
-0 3 0x0000000000010004 (0x00050663)
-0 3 0x0000000000010010 (0x00000597) x11 0x0000000000010010
-0 3 0x0000000000010014 (0x0f058593) x11 0x0000000000010100
-0 3 0x0000000000010018 (0x60300413) x 8 0x0000000000000603
-0 3 0x000000000001001c (0x7b041073)
-0 3 0x0000000000010020 (0x0010041b) x 8 0x0000000000000001
-0 3 0x0000000000010024 (0x01f41413) x 8 0x0000000080000000
-0 3 0x0000000000010028 (0x7b141073)
-0 3 0x000000000001002c (0x7b200073)"
-         );
-    end
-
     string _elf = "/opt/riscv/target/share/riscv-tests/isa/rv64ui-p-xor";
-
-    initial begin
-        dromajo_cosim(_elf);
-    end
 
 `define dromajo             "/opt/riscv/bin/dromajo"
 `define dromajo_cosim_test  "/opt/riscv/bin/dromajo_cosim_test"
 `define objcopy             "/opt/riscv/bin/riscv64-unknown-elf-objcopy"
-
-    task dromajo_cosim(string elf);
-        $system({`dromajo, " --trace 0 ", elf, " 2> check.trace"});
-        $system({`objcopy, " -O verilog ", elf, " temp.hex"});
-        read_hex("temp.hex");
-    endtask
-
 
     class Exception;
         bit [63:0] cause;
@@ -170,7 +142,8 @@ module tb_hart;
             this.tval = tval;
         endfunction
 
-        virtual function what();
+        virtual function string what();
+            return "";
         endfunction
 
     endclass
@@ -183,16 +156,16 @@ module tb_hart;
             this.csr_addr = csr_addr;
         endfunction
 
-        function what();
-            $fdisplay(
-                fd,
-                "csr_read: invalid CSR=0x%-h",
-                csr_addr
+        function string what();
+            return $sformatf(
+                "csr_read: invalid CSR=0x%-h\n",
+                this.csr_addr
             );
         endfunction
+
     endclass
 
-    bit [31:0] ir_retired;
+    enum {IF, PD, ID, EX, MEM, WB} phase;
 
     class Instruction;
         bit [31:0] ir;
@@ -213,12 +186,11 @@ module tb_hart;
             this.e = null;
         endfunction
 
-        function retire();
+        function string retire();
             if(this.e) begin
-                this.e.what();
+                string exc = this.e.what();
 
-                $fdisplay(
-                    fd,
+                string ret = $sformatf(
                     "%-d %-d 0x%16h (0x%8h) exception %-d, tval %16h",
                     this.hart_id,
                     this.priv_lvl,
@@ -227,10 +199,11 @@ module tb_hart;
                     this.e.cause,
                     this.e.tval
                 );
+
+                return {exc, ret};
             end
             else if(dut.wr && dut.rd) begin
-                $fdisplay(
-                    fd,
+                return $sformatf(
                     "%-d %-d 0x%16h (0x%8h) x%2d 0x%16h",
                     this.hart_id,
                     this.priv_lvl,
@@ -241,64 +214,225 @@ module tb_hart;
                 );
             end
             else begin
-                $fdisplay(
-                    fd,
+                return $sformatf(
                     "%-d %-d 0x%16h (0x%8h)",
                     this.hart_id,
                     this.priv_lvl,
                     this.pc,
-                    this.ir,
+                    this.ir
                 );
             end
-            ir_retired = this.ir;
         endfunction
 
     endclass
 
-    enum {IF, PD, ID, EX, MEM, WB} phase;
-    Instruction pipeline [1:5];
+    class Test;
 
-    always @(posedge clk) begin
-        if(!dut.stall_wb && pipeline[WB]) pipeline[WB].retire();
+        string name;
 
-        if(!dut.stall_mem) pipeline[WB]  = pipeline[MEM];
-        if(!dut.stall_ex)  pipeline[MEM] = pipeline[EX];
-        if(!dut.stall_id)  pipeline[EX]  = pipeline[ID];
-        if(!dut.stall_pd)  pipeline[ID]  = pipeline[PD];
-        if(!dut.stall_if)  pipeline[PD]  = new(0, dut.u_csr.privilege_level, dut.ir, dut.pc);
+        function new(string elf);
+            integer k;
+            this.elf = elf;
+            for(integer i = 0; i < elf.len(); ++i) begin
+                if(elf.getc(i) == 8'h2F) begin
+                    k = i;
+                end
+            end
+            this.name = elf.substr(k+1, elf.len()-1);
+        endfunction
 
-        if(dut.t_flush_mem && !pipeline[MEM].e) pipeline[MEM] = null;
+        local string elf;
 
-        if(dut.t_flush_ex)  pipeline[EX] = null;
+        local bit [31:0] ir_retired;
+        local integer fd;
+        local Instruction pipeline [1:5];
 
-        if(dut.t_flush_id || dut.flush_id) begin
-            pipeline[ID] = null;
-        end
+        local task dromajo_cosim();
+            //$system({`dromajo, " --trace 0 ", this.elf, " 2> check.trace"});
+            $system({`objcopy, " -O verilog ", this.elf, " temp.hex"});
+            read_hex("temp.hex");
 
-        if(dut.t_flush_pd || dut.flush_pd) begin
-            pipeline[PD] = null;
-        end
-    end
+            // probably some debug section or some other kind of bootrom?? not documented anywhere
+            // fdisplay here just so trace comparison works
+            $fdisplay(
+                this.fd,
+"0 3 0x0000000000010000 (0xf1402573) x10 0x0000000000000000
+0 3 0x0000000000010004 (0x00050663)
+0 3 0x0000000000010010 (0x00000597) x11 0x0000000000010010
+0 3 0x0000000000010014 (0x0f058593) x11 0x0000000000010100
+0 3 0x0000000000010018 (0x60300413) x 8 0x0000000000000603
+0 3 0x000000000001001c (0x7b041073)
+0 3 0x0000000000010020 (0x0010041b) x 8 0x0000000000000001
+0 3 0x0000000000010024 (0x01f41413) x 8 0x0000000080000000
+0 3 0x0000000000010028 (0x7b141073)
+0 3 0x000000000001002c (0x7b200073)"
+            );
 
-    always @(negedge clk) begin
-        if(dut.u_csr.csr_addr_invalid && pipeline[MEM]) begin
-            automatic InvalidCSRException ex = new(2, 0, dut.u_csr.csr_addr);
-            pipeline[MEM].e = ex;
-        end
+        endtask
 
-        if(dut.u_csr.ecall && pipeline[MEM]) begin
-            automatic Exception ex = new(dut.u_csr.cause, dut.u_csr.val);
-            pipeline[MEM].e = ex;
-        end
-    end
+        local task pipeline_sync();
+            forever begin
+                if(!this.fd) break;
+                @(posedge clk) begin
+                    if(!dut.stall_wb && this.pipeline[WB]) begin
+                        this.ir_retired = this.pipeline[WB].ir;
+                        if(!this.fd) break;
+                        $fdisplay(
+                            this.fd,
+                            "%s",
+                            this.pipeline[WB].retire()
+                        );
+                    end
 
-    always @(negedge clk) begin
-        // end sim if write_tohost
-        if(ir_retired == 32'hfc3f2223) begin
+                    if(!dut.stall_mem) this.pipeline[WB]  = this.pipeline[MEM];
+                    if(!dut.stall_ex)  this.pipeline[MEM] = this.pipeline[EX];
+                    if(!dut.stall_id)  this.pipeline[EX]  = this.pipeline[ID];
+                    if(!dut.stall_pd)  this.pipeline[ID]  = this.pipeline[PD];
+                    if(!dut.stall_if)  this.pipeline[PD]  = new(0, dut.u_csr.privilege_level, dut.ir, dut.pc);
+
+                    if(dut.t_flush_mem && !this.pipeline[MEM].e) this.pipeline[MEM] = null;
+
+                    if(dut.t_flush_ex)  this.pipeline[EX] = null;
+
+                    if(dut.t_flush_id || dut.flush_id) begin
+                        this.pipeline[ID] = null;
+                    end
+
+                    if(dut.t_flush_pd || dut.flush_pd) begin
+                        this.pipeline[PD] = null;
+                    end
+                end
+            end
+        endtask
+
+        local task exception_handler();
+            forever begin
+                if(!this.fd) break;
+                @(negedge clk) begin
+                    if(dut.u_csr.csr_addr_invalid && this.pipeline[MEM]) begin
+                        automatic InvalidCSRException ex = new(2, 0, dut.u_csr.csr_addr);
+                        this.pipeline[MEM].e = ex;
+                    end
+                    if(dut.u_csr.ecall && this.pipeline[MEM]) begin
+                        automatic Exception ex = new(dut.u_csr.cause, dut.u_csr.val);
+                        this.pipeline[MEM].e = ex;
+                    end
+                end
+            end
+        endtask
+
+        bit passed;
+
+        local task tohost_monitor();
+            forever begin
+                @(negedge clk) begin
+                    if(this.ir_retired == 32'hfc3f2223) begin
+                        $fclose(this.fd);
+                        this.fd = 0;
+                        this.passed = $system({`dromajo_cosim_test, " cosim ", this.name, ".trace ", this.elf, " > /dev/null 2> /dev/null"}) == 0;
+                        break;
+                    end
+                end
+            end
+        endtask
+
+        local task timeout();
+            #200_0000;
+        endtask
+
+        task run();
+
+            // hart reset signal
+            #80
+            h_rst_n = 0;
+            #80;
+            h_rst_n = 1;
+
+            // trace file handle init
+            this.fd = $fopen({this.name, ".trace"}, "w");
+
+            // run co-sim
+            this.dromajo_cosim();
+            fork
+                this.pipeline_sync();
+                this.exception_handler();
+                this.tohost_monitor();
+                this.timeout();
+            join_any
+        endtask
+
+    endclass
+
+    class RiscvTestEnv;
+
+        string path;
+
+        function new(string path, string template);
+            this.path = path;
+            this.gen_file_list(template);
+            this.passed = 0;
+            this.failed = 0;
+        endfunction
+
+        local task gen_file_list(string template);
+            $system({"find ", this.path, " -name '", template, "' -not -name '*.dump' > tb_hart.lst"});
+        endtask
+
+        Test t;
+
+        integer passed;
+        integer failed;
+
+        task run();
+            integer fd;
+            string filename;
+
+            $display("Running riscv-tests...");
+            fd = $fopen("tb_hart.lst", "r");
+            while(!$feof(fd)) begin
+                $fgets(filename, fd);
+                filename = filename.substr(0, filename.len()-2);
+                if(filename == "") break;
+                t = new(filename);
+                t.run();
+                $display(
+                    "%-25s: %s",
+                    t.name,
+`ifdef ANSI_COLORS
+                    t.passed ? "\x1b[1;32mpassed\x1b[0m" : "\x1b[1;31mfailed\x1b[0m"
+`else
+                    t.passed ? "passed" : "failed"
+`endif
+                );
+
+                this.passed +=  t.passed;
+                this.failed += !t.passed;
+            end
+
+            $display(
+`ifdef ANSI_COLORS
+                "riscv-tests finished: \x1b[1;32m%d\x1b[0m passed, \x1b[1;31m%d\x1b[0m failed!",
+`else
+                "riscv-tests finished: %d passed, %d failed!",
+`endif
+                this.passed,
+                this.failed
+            );
+
             $fclose(fd);
-            $system({`dromajo_cosim_test, " cosim trace ", _elf});
-            $stop();
-        end
+
+            $system("rm tb_hart.lst");
+
+        endtask
+
+    endclass
+
+    RiscvTestEnv env;
+
+    initial begin
+        env = new("/opt/riscv/target/share/riscv-tests/isa/", "rv64ui-p-*");
+        env.run();
+        $stop();
     end
 
 endmodule
