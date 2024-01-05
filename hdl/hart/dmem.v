e4eb44f476cdf9cf6368306eec3d9d572725f602
kiclu <kiclu@protonmail.com>
2024-01-06 00:59:35 +0100

    copyright update

--- hdl/hart/dmem.v
+++ hdl/hart/dmem.v
@@ -1,20 +1,18 @@
-/*
- * Copyright (C) 2023  Nikola Lukic <lukicn@protonmail.com>
- * This source describes Open Hardware and is licensed under the CERN-OHL-W v2
+/* Copyright (C) 2024  Nikola Lukić <lukicn@protonmail.com>
+ * This source describes Open Hardware and is licensed under the CERN-OHL-S v2
  *
  * You may redistribute and modify this documentation and make products
- * using it under the terms of the CERN-OHL-W v2 (https:/cern.ch/cern-ohl).
+ * using it under the terms of the CERN-OHL-S v2 (https:/cern.ch/cern-ohl).
  * This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED
  * WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY
- * AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN-OHL-W v2
+ * AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN-OHL-S v2
  * for applicable conditions.
  *
  * Source location: https://www.github.com/kiclu/rv6
  *
- * As per CERN-OHL-W v2 section 4.1, should You produce hardware based on
+ * As per CERN-OHL-S v2 section 4.1, should You produce hardware based on
  * these sources, You must maintain the Source Location visible on the
- * external case of any product you make using this documentation.
- */
+ * external case of any product you make using this documentation. */
 
 /*
  * Configurable, write-allocated L1 data cache with write-through policy
